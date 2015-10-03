2a3dc90c8d9d4515c020fe32c4dab89b1e511dce
Igor Stepanov <sigorilla@gmail.com>
2015-10-03 14:50:09 +0300

    change module for markdown

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-var showdown  = require('showdown');
-var markDownConverter = new showdown.Converter();
+var marked  = require('marked');
+marked.setOptions({
+  tables: false,
+  sanitize: true,
+});
 
 module.exports = function (messages) {
   try {
     return messages.map(function (message) {
       var mdMessage = message;
-      mdMessage.text = markDownConverter.makeHtml(message.text);
+      mdMessage.text = marked(message.text);
       return mdMessage;
     });
   } catch (err) {
